96c9530e1aad042f68e6c8934d58784a7ed7d484
martingale708 <nyainahr@gmail.com>
2024-08-22 18:30:58 +0200

    deuxieme commit

--- src/components/darkmode/darkmode.jsx
+++ src/components/darkmode/darkmode.jsx
@@ -1,33 +1,3 @@
-// import { useTheme } from '../../utils/hooks'
-// import { ReactComponent as Sun } from '../../assets/darkmode/sun.svg'
-// import { ReactComponent as Moon } from '../../assets/darkmode/moon.svg'
-// import '../../sass/components/_darkmode.scss'
-
-// const DarkMode = () => {
-//   const { darkMode, toggleDarkMode } = useTheme()
-
-//   return (
-//     <div className="header__darkmode">
-//       <input
-//         className="header__darkmode-input"
-//         type="checkbox"
-//         id="darkmode-toggle"
-//         onChange={toggleDarkMode}
-//         defaultChecked={darkMode}
-//       />
-//       <label
-//         className="header__darkmode-label"
-//         htmlFor="darkmode-toggle"
-//         aria-label="changer le thème du site"
-//       >
-//         <Sun />
-//         <Moon />
-//       </label>
-//     </div>
-//   )
-// }
-
-// export default DarkMode
 import { useTheme } from '../../utils/hooks'
 import { FiSun, FiMoon } from 'react-icons/fi'
 import '../../sass/components/_darkmode.scss'
--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useTheme } from '../../utils/hooks/index'
-import LogoMb from '../../assets/logo/logo1.png'
-import DarkLogoMb from '../../assets/logo/logo2.png'
+import Logolight from '../../assets/logo/logo1.png'
+import Logodark from '../../assets/logo/logo2.png'
 import '../../sass/components/_logo.scss'
 
 const Logo = () => {
@@ -9,7 +9,7 @@ const Logo = () => {
   return (
     <div className="header__logo">
       <Link to="/">
-        <img src={darkMode ? DarkLogoMb : LogoMb} alt="Mon logo personnel" />
+        <img src={darkMode ? Logodark : Logolight} alt=" logo personnel" />
       </Link>
     </div>
   )
