5dfa8e464fb8741b65298b3dae0a1afd31d5d59c
martingale708 <nyainahr@gmail.com>
2024-08-27 10:43:08 +0200

    correction comportement mobile

--- public/scripts/card-toggle.js
+++ public/scripts/card-toggle.js
@@ -1,5 +1,15 @@
 document.querySelectorAll('.grid__project__card').forEach(card => {
-    card.addEventListener('click', () => {
+    card.addEventListener('click', (event) => {
+      event.preventDefault(); // Empêche le comportement par défaut
+  
+      // Supprime la classe 'active' de toutes les cartes sauf celle qui est cliquée
+      document.querySelectorAll('.grid__project__card').forEach(c => {
+        if (c !== card) {
+          c.classList.remove('active');
+        }
+      });
+  
+      // Ajoute ou supprime la classe 'active' à la carte cliquée
       card.classList.toggle('active');
     });
   });
